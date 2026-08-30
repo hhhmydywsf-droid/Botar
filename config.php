a1da0895360bfd9e8d75ef4cb86d76ba0234b2da
hhhmydywsf-droid <hhhmydywsf@gmail.com>
2026-08-30 22:23:58 +0300

    Update config.phpmmmm

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
-$TOKEN = ""; // bot token
-$botUsername = ""; // bot username without @
+$TOKEN = "8645637119:AAFrO1zSBozTLxxVRTt7WQKcYh2RebswKjU"; // bot token
+$botUsername = "@YUTYIBOT"; // bot username without @
 
 /*
 $databaseHost = "localhost";
@@ -9,6 +9,6 @@ $databaseUsername = ""; // db username
 $databasePassword = ""; // db password
 */
 
-$sudoID = ""; // sudo id
+$sudoID = "8600983125"; // sudo id
 
 date_default_timezone_set("Asia/Riyadh");
